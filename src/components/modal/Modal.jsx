293fae9e03a7f903d2c050195fd5537ef5930369
AlexKat <katsubo79@gmail.com>
2023-12-06 12:19:12 +0200

    1

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './Modal.module.css';
+import { Icon } from 'components/img/Icon';
+
+export const Modal = () => (
+  <div>
+    <p>sdfsdf</p>
+    <Icon id="user-plus" className={css.icoses} />
+    <p>sfsdf</p>
+    <div className={css.ises}></div>
+  </div>
+);
